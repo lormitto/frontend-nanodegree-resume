2bd87cef1feea23c138a4d5bf1456fe14a8dbcd7
Nowicki Adam <adam.nowicki@nsn.com>
2015-12-05 22:18:13 +0100

    Notes added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,12 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append(["Adamek Jakistam"]);
+ var formattedName = HTMLheaderName.replace("%data%", "Adam Nowicki");
+ var formattedRole = HTMLheaderRole.replace("%data%", "R&D Manager");
+
+ $("#header").prepend(formattedName);
+ $("#header").prepend(formattedRole);
+
+ var skills = ["awesomeness", "programming"];
+
+$("#main").append(skills.length);
